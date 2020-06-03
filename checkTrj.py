fdfbfa02453d9eaf7f39cd69ec56e954ec4d35f4
Yuki Fujii <yfujii@post.kek.jp>
2020-06-03 21:10:25 +1000

    Minor mod.

--- checkTrj.py
+++ checkTrj.py
@@ -6,6 +6,24 @@ import math
 
 import matplotlib.pyplot as plt
 
+pNames=['e-','e+','mu-','mu+','pi-','pi+','n','p','others']
+pdgCodes=[11,-11,13,-13,-211,211,22,2112,2212,'others']
+def makeBarVals(data):
+    values = np.zeros(len(pdgCodes))
+    for i in range(len(data)):
+        if   data[i]==pdgCodes[0]: values[0]=values[0]+1
+        elif data[i]==pdgCodes[1]: values[1]=values[1]+1
+        elif data[i]==pdgCodes[2]: values[2]=values[2]+1
+        elif data[i]==pdgCodes[3]: values[3]=values[3]+1
+        elif data[i]==pdgCodes[4]: values[4]=values[4]+1
+        elif data[i]==pdgCodes[5]: values[5]=values[5]+1
+        elif data[i]==pdgCodes[6]: values[6]=values[6]+1
+        elif data[i]==pdgCodes[7]: values[7]=values[7]+1
+        elif data[i]==pdgCodes[8]: values[8]=values[8]+1
+        else :                     values[9]=values[9]+1
+    
+    return values
+
 def main(fname):
     ttree = uproot.open(fname)["trjTree"]
     print(ttree.keys())
@@ -13,7 +31,7 @@ def main(fname):
     print(ptree.keys())
     
     trj_pId, trj_nhits, trj_pos, trj_mom = ttree.arrays(["pId", "numHits", "pos", "mom"], outputtype=tuple)
-    prt_pId, prt_pos, prt_mom, prt_stop = ptree.arrays(["pId", "pos", "mom", "posF"], outputtype=tuple)
+    prt_pId, prt_pos, prt_mom, prt_stop, prt_lvl = ptree.arrays(["pId", "pos", "mom", "posF", "pLevel"], outputtype=tuple)
     print("#of trajectories: ",len(trj_pId))
 
     prt_pos_ent = prt_pos[prt_pos.x<3500]
@@ -25,15 +43,20 @@ def main(fname):
     tposz=np.array(trj_pos.z-7650.0)
     tpost=np.array(trj_pos.t)
     
-    pposx=np.array(prt_pos.x)
-    pposy=np.array(prt_pos.y)
-    pposz=np.array(prt_pos.z-7650.0)
-    ppost=np.array(prt_pos.t)
+    pposx=np.array(prt_pos[prt_lvl==1].x)
+    pposy=np.array(prt_pos[prt_lvl==1].y)
+    pposz=np.array(prt_pos[prt_lvl==1].z-7650.0)
+    ppost=np.array(prt_pos[prt_lvl==1].t)
 
     pposx2=np.array(prt_pos_ent.x)
     pposy2=np.array(prt_pos_ent.y)
     pposz2=np.array(prt_pos_ent.z-7650.0)
     ppost2=np.array(prt_pos_ent.t)
+
+    gposx=np.array(prt_pos[prt_lvl==2].x)
+    gposy=np.array(prt_pos[prt_lvl==2].y)
+    gposz=np.array(prt_pos[prt_lvl==2].z-7650.0)
+    gpost=np.array(prt_pos[prt_lvl==2].t)
     
     plt.figure(figsize=(12,8))
     x_bins=[np.linspace( 3100, 9100,120),
@@ -43,6 +66,7 @@ def main(fname):
     #plt.hist2d(tposx,tposz,bins=[x_bins[0],y_bins[0]])
     plt.scatter(tposx,tposz,s=8,label='Trajectories')
     plt.scatter(pposx,pposz,s=8,label='Parents'     )
+    plt.scatter(gposx,gposz,s=8,label='Grandparents')
     plt.title('Z-X 2D for origin of triggered trajectories')
     plt.xlim( 3100, 9100)
     plt.ylim(-1500, 1500)
@@ -54,6 +78,7 @@ def main(fname):
     plt.title('Z origin')
     plt.hist(tposx,bins=zbins,label='Trajectories')
     plt.hist(pposx,bins=zbins,label='Parents'     )
+    plt.hist(gposx,bins=zbins,label='Grandparents')
     plt.xlim(3100,9100)
     plt.legend(loc='upper right')
     plt.show()
@@ -61,6 +86,7 @@ def main(fname):
     plt.figure(figsize=(8,8))
     plt.scatter(tposz,tposy,s=8,label='Trajectories')
     plt.scatter(pposz,pposy,s=8,label='Parents'     )
+    plt.scatter(gposz,gposy,s=8,label='Grandparents')
     plt.title('X-Y 2D for origin of triggered trajectories')
     plt.xlim(-1500, 1500)
     plt.ylim(-1500, 1500)
@@ -83,22 +109,23 @@ def main(fname):
     plt.figure(figsize=(12,8))
     plt.title('Initial momentum distributions of trajectories and their parents')
     plt.hist(trj_mom.p,bins=pbins,label='Trajectories')
-    plt.hist(prt_mom.p,bins=pbins,label='Parents'     )
+    plt.hist(prt_mom[prt_lvl==1].p,bins=pbins,label='Parents'     )
     plt.hist(prt_mom_ent.p,bins=pbins,label='Parents@Ent')
     plt.xlim(0,200)
     plt.legend(loc='upper right')
     plt.ylabel('Entries/MeV')
     plt.xlabel('Momentum [MeV/c]')
     plt.show()
-#
-#    plt.figure(figsize=(12,8))
-#    plt.title('Pz')
-#    plt.hist(trj_mom.x,bins=pbins,label='Trajectories')
-#    plt.hist(prt_mom.x,bins=pbins,label='Parents'     )
-#    plt.hist(prt_mom_ent.x,bins=pbins,label='Parents@Ent')
-#    plt.xlim(0,200)
-#    plt.legend(loc='upper right')
-#    plt.show()
+
+    cbins=np.linspace(-1.1,1.1,100)
+    plt.figure(figsize=(12,8))
+    plt.title('cos')
+    plt.hist(trj_mom.x/trj_mom.p,bins=cbins,label='Trajectories')
+    plt.hist(prt_mom[prt_lvl==1].x/prt_mom[prt_lvl==1].p,bins=cbins,label='Parents'     )
+    plt.hist(prt_mom_ent.x/prt_mom_ent.p,bins=cbins,label='Parents@Ent')
+    plt.xlim(-1.1,1.1)
+    plt.legend(loc='upper right')
+    plt.show()
 
     # Estimate the 2D histogram
     nbins = 50
@@ -121,13 +148,28 @@ def main(fname):
     plt.show()
 
     plt.figure(figsize=(12,4))
-    plt.hist(trj_pId,bins=np.linspace(-24.5,24.5,50),label='Trajectories')
-    plt.hist(prt_pId,bins=np.linspace(-24.5,24.5,50),label='Parents'     )
-    plt.hist(prt_pId_ent,bins=np.linspace(-24.5,24.5,50),label='Parents@Ent')
-    plt.title('PID of each trajectories associated to the trigger')
-    plt.legend(loc='upper left')
-    plt.xlabel('PDG code')
+    left = np.arange(len(pdgCodes))
+    width= 0.25
+    label='Triggered trajectories'
+    values1=makeBarVals(trj_pId)
+    plt.bar(left,        values1,width=width,color='blue', label=label)
+    label='Parents of trg. trjs.'
+    values2=makeBarVals(prt_pId[prt_lvl==1])
+    plt.bar(left+width,  values2,width=width,color='red',  label=label)
+    label='Grandparents of trg. trjs.'
+    values3=makeBarVals(prt_pId[prt_lvl==2])
+    plt.bar(left+2*width,values3,width=width,color='green',label=label)
+    plt.xticks(left+width/2, pdgCodes)
+    plt.ylim(0.5,len(trj_pId))
+    plt.grid(True)
     plt.yscale('log')
+    plt.title('PID of each trajectories associated to the trigger')
+    plt.legend(loc='upper right')
+    plt.ylabel('Number of entries')
+    plt.xlabel('PDG code')
     plt.show()
 
-    #print(prt_pId)
+    print('Parents PID')
+    print(prt_pId[prt_lvl==1])
+    print('Grandparents PID')
+    print(prt_pId[prt_lvl==2])
