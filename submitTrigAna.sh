4f9d4171d5482888a110bede49b5e269e9a96124
Yuki Fujii <yfujii@post.kek.jp>
2020-08-26 16:26:06 +0900

    some modifications.

--- submitTrigAna.sh
+++ submitTrigAna.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-NumRun=92
-for(( set=0; set<4; set++))
+#NumRun=182
+NumRun=186
+#for(( set=0; set<6; set++))
+for(( set=5; set<6; set++))
 do
+    if [ $set -eq 3 ]; then
+        continue
+    else
 #   while : ; do
 #     NJobs=$(ps aux |grep "python3 runTrig.py" |wc -l)
 #     if [ $NJobs -le 1 ];then
@@ -13,12 +18,20 @@ do
 #   done
    for thr in 150 250 350 450 550 650 750 850 950 1050
    do
-      log=/group/had/muon/yfujii/work/trig/out${set}_thr${thr}keV_deltaSegIsOne.txt
+      log=/group/had/muon/yfujii/work/trig/out${set}_thr${thr}keV_deltaSegIsOneScintOnly.txt
       if [ -f $log ];then
         rm -f $log
       fi
       #python3 runTrig.py  0 84 $set $thr > out${set}_thr${thr}keV_deltaSegIsOne.txt &
       #python3 runTrig.py  0 $NumRun $set $thr > out${set}_thr${thr}keV_deltaSegIsOne.txt &
-      bsub -o $log -q s python3 runTrig.py 0 $NumRun $set $thr "/group/had/muon/yfujii/work/decode/"
+      bsub -o $log -q l python3 runTrig.py 0 $NumRun $set $thr "/group/had/muon/yfujii/work/decode/" 1
+      if [ $set -eq  0  -o  $set -eq 1 ]; then
+        log=/group/had/muon/yfujii/work/trig/out${set}_thr${thr}keV_deltaSegIsOneUseCheren.txt
+        if [ -f $log ];then
+          rm -f $log
+        fi
+        bsub -o $log -q l python3 runTrig.py 0 $NumRun $set $thr "/group/had/muon/yfujii/work/decode/" 0
+      fi
    done
+   fi
 done
